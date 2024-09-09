4ebd0b40301ff3797f78c7e9240cadb5120df824
Abhay Singh <singhchirag188@gmail.com>
2024-09-09 22:40:42 +0530

    Added Post Routes -> Getting all posts -> Create new post -> Update a given post

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,18 +1,30 @@
 import { PrismaClient } from '@prisma/client/edge'
 import { withAccelerate } from '@prisma/extension-accelerate'
-import { Hono } from 'hono';
+import { Hono,Context } from 'hono';
 import { env } from 'hono/adapter';
 import { z } from 'zod';
 import { sign, verify } from 'hono/jwt'
 
-const app = new Hono().basePath('/api/v1');
+type Variables = {
+  userId?: string
+}
+type Bindings = {
+  DATABASE_URL: string
+  JWT_SECRET: string
+}
 
-//development routes
-app.get('/development/users',async (c)=>{
+const app = new Hono<{ Bindings: Bindings, Variables: Variables }>().basePath('/api/v1');
+
+//welcome route
+app.get('/',(c)=>{
+  return c.json({success: true,message: 'Welcome to the API'});
+});
+
+//user routes
+app.get('/user/development',async (c)=>{
   try{
-    const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
     const prisma = new PrismaClient({
-      datasourceUrl: DATABASE_URL
+      datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate());
 
     const res = await prisma.user.findMany();
@@ -26,21 +38,10 @@ app.get('/development/users',async (c)=>{
   }
 })
 
-app.get('',(c)=>{
-  return c.json({success: true,message: 'Welcome to the API'});
-});
-
-app.get('/url',(c)=>{
-  const { DATABASE_URL } = env<{ DATABASE_URL: string }>(c);
-
-  return c.json({success: true, url: DATABASE_URL});
-});
-
-app.post('/signup',async (c)=>{
+app.post('user/signup',async (c)=>{
   try{
-    const { DATABASE_URL,JWT_SECRET } = env<{ DATABASE_URL: string,JWT_SECRET: string }>(c);
     const prisma = new PrismaClient({
-      datasourceUrl: DATABASE_URL
+      datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate());
 
     const body = await c.req.parseBody();
@@ -81,7 +82,7 @@ app.post('/signup',async (c)=>{
       }
     });
 
-    const token = await sign({id:newUser.id},JWT_SECRET);
+    const token = await sign({id:newUser.id},c.env.JWT_SECRET);
 
     c.status(201);
     return c.json({success: true, token});
@@ -92,11 +93,10 @@ app.post('/signup',async (c)=>{
   }
 });
 
-app.post('/login',async (c)=>{
+app.post('user/login',async (c)=>{
   try{
-    const { DATABASE_URL,JWT_SECRET } = env<{ DATABASE_URL: string,JWT_SECRET: string }>(c);
     const prisma = new PrismaClient({
-      datasourceUrl: DATABASE_URL
+      datasourceUrl: c.env.DATABASE_URL
     }).$extends(withAccelerate());
 
     const body = await c.req.parseBody();
@@ -127,7 +127,7 @@ app.post('/login',async (c)=>{
       return c.json({ success: false, error: 'Invalid Credentials' });
     }
 
-    const token = await sign({id:user.id},JWT_SECRET);
+    const token = await sign({id:user.id},c.env.JWT_SECRET);
 
     c.status(200);
     return c.json({success: true, token});
@@ -138,19 +138,20 @@ app.post('/login',async (c)=>{
   }
 });
 
-app.use('/blog/*',async (c,next)=>{
+//posts routes
+app.use('/post/*',async (c,next)=>{
   try{
-    const { JWT_SECRET } = env<{ JWT_SECRET: string }>(c);
     const token = c.req.header("authorization");
 
     if(!token){
       c.status(403);
       return c.json({success: false,error: 'Unauthorized'});
-    }
+    };
 
-    const decoded = await verify(token,JWT_SECRET);
+    const decoded = await verify(token,c.env.JWT_SECRET);
 
     if(decoded.id){
+      c.set('userId', String(decoded.id));
       await next();
     }
     else {
@@ -164,6 +165,117 @@ app.use('/blog/*',async (c,next)=>{
   }
 });
 
+app.get('/post/all',async (c)=>{
+  try{
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+
+    const res = await prisma.post.findMany();
+
+    c.status(200);
+    return c.json({success: true, posts:res});
+  }
+  catch(err){
+    c.status(500);
+    return c.json({success: false,error: 'Internal Server Error'});
+  }
+});
+
+app.post('/post',async (c)=>{
+  try{
+      const prisma = new PrismaClient({
+        datasourceUrl: c.env.DATABASE_URL
+      }).$extends(withAccelerate());
+  
+      const body = await c.req.parseBody();
+  
+      // Define schema
+      const schema = z.object({
+        title: z.string().min(3),
+        content: z.string()
+      });
+  
+      // Validate input using schema
+      const parsedData = schema.safeParse(body);
+  
+      if (!parsedData.success) {
+        c.status(400);
+        return c.json({ success: false, error: 'Invalid Data Provided' });
+      }
+  
+      const { title, content } = parsedData.data;
+      const userId = c.get("userId") || "";
+  
+      //add new post
+      const npost = await prisma.post.create({
+          data:{
+              title,
+              content,
+              authorId: userId
+          }
+      });
+  
+      c.status(201);
+      return c.json({success: true, npost});
+    }
+    catch(err){
+      c.status(500);
+      return c.json({success: false,error: 'Internal Server Error'});
+    }
+});
+
+app.put('/post',async (c)=>{
+  try{
+    const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL
+    }).$extends(withAccelerate());
+    
+    const userId = c.get("userId") || "";
+    const body = await c.req.parseBody();
+
+    // Define schema
+    const schema = z.object({
+      id: z.string(),
+      title: z.string().min(3),
+      content: z.string()
+    });
+
+    // Validate input using schema
+    const parsedData = schema.safeParse(body);
+
+    if (!parsedData.success) {
+      c.status(400);
+      return c.json({ success: false, error: 'Invalid Data Provided' });
+    }
+
+    const { id, title, content } = parsedData.data;
+
+    const post = await prisma.post.update({
+      where: { 
+        id,
+        authorId: userId
+      },
+      data: {
+        title,
+        content
+      }
+    });
+
+    if(!post){
+      c.status(404);
+      return c.json({success: false,error: 'Post not found or Unauthorized'});
+    }
+
+    c.status(200);
+    return c.json({success: true, post});
+  }
+  catch(e){
+    console.log(e);
+    c.status(500);
+    return c.json({success: false,error: 'Internal Server Error'});
+  }
+});
 
 
 export default app;
